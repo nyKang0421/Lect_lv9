5f62dd2a1d85946aa20827b4083fdf2b27a2a035
naeyeon Kang <ganggom2@gmail.com>
2025-01-15 17:45:27 +0900

    feat: add controller package

--- level9/src/main/Main.java
+++ level9/src/main/Main.java
@@ -1,10 +1,14 @@
 package main;
 
+import controller.Controller;
+
 public class Main {
 	public static void main(String[] args) {
 		
 		
 		System.out.println("메인");
+		Controller cl = new Controller();
+		
 	}
 
 }
